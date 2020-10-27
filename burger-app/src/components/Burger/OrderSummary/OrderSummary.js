07d248b04f76a31ea3771059803747beb247d26d
Michaël De Wree <michael_de.wree@live.be>
2020-10-27 11:05:10 +0100

    Adding Custom Button Component

--- burger-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -12,6 +12,9 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p>Continue to checkout?</p>
+            <button>CANCEL</button>
+            <button>CONTINUE</button>
         </Aux>
     )
 };
